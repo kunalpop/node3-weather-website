4e2b8b028f404c43863ca745209378ee1c7abaad
Kunal Soni <kunalpop@gmail.com>
2022-11-17 18:30:15 +0530

    Set up app for Heorku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const { request } = require('http')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -85,6 +86,6 @@ app.get('*',(req,res) => {
     })
 })
 
-app.listen(3000, ()=> {
-    console.log("Server is up at port 3000.")
+app.listen(port, ()=> {
+    console.log("Server is up at port "+port)
 })
